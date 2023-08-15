bf03ac5c190ea27eec712231ebd302e9e997c51a
OydinJurayeva <oydinjorayeva1020@gmail.com>
2023-08-15 10:09:27 +0500

    new

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+// style
+import "./Footer.css"
+
+function Footer() {
+   return (
+      <div className="footer">
+         <div className="footer-container container">
+            <p>All right reserved</p>
+            <a href="#" target="_blank">Oydin</a>
+         </div>
+      </div>
+   )
+}
+
+export default Footer
